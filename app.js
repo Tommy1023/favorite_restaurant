25beab7e8e16e40751ba1551359387e3f67eb414
Tommy1023 <j.h.chen1023@gmail.com>
2022-01-09 01:24:37 +0800

    Add Express-handlebars Set view engine Add main.handlebars & index.handlebars

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+//require express modules
 const express = require('express')
 const app = express()
 const port = 3000
 
+//require express-handlebars
+const exphbs = require('express-handlebars')
+
 app.get('/', (req, res) => {
-  res.send('My Favorite restaurant')
+  res.render('index')
 })
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//setting static files
+app.use(express.static('public'))
