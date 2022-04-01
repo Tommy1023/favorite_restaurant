20193c340cd1109b9ddf680211b55344a0bb1fb8
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-01 17:16:20 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const passport = require('passport')
+const { rawListeners } = require('../../models/user')
 const router = express.Router()
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
-})
-
-router.post('/login', (req, res) => {
-  const { email, password } = req.body
 })
 
 router.get('/register', (req, res) => {
@@ -54,5 +51,10 @@ router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/users/login'
 }))
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
 
 module.exports = router
